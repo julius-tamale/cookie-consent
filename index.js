974115b9255cebbe21a54daebc7a50269c4a14ae
julius-tamale <juliotamex@proton.me>
2023-04-18 22:17:30 +0200

    cookie consent

--- index.js
+++ index.js
@@ -3,15 +3,22 @@ const modalCloseBtn = document.getElementById('modal-close-btn');
 const consentForm = document.getElementById('consent-form');
 const modalText = document.getElementById('modal-text')
 const modalInner = document.getElementById('modal-inner');
+const modalChoiceBtns = document.getElementById('modal-choice-btns')
+const declineBtn = document.getElementById('decline-btn')
 // const acceptBtn = document.getElementById('accept-btn')
 
+setTimeout( () => {
+    modalSelector.style.display = 'inline';
+}, 2000)
+
 modalCloseBtn.addEventListener('dblclick', () => {
-    modalSelector.style.display = 'none';
+    modalSelector.style.display = 'none'
 })
 
-setTimeout( () => {
-    modalSelector.style.display = 'block';
-}, 2000)
+declineBtn.addEventListener('mouseenter', function(){
+     modalChoiceBtns.classList.toggle('reverse')
+})
+
 
 consentForm.addEventListener('submit', function(e){
     e.preventDefault()
@@ -20,12 +27,12 @@ consentForm.addEventListener('submit', function(e){
 
 
     modalText.innerHTML = `
-    <div class="modal-inner-loading">
-        <img src="images/loading.svg" class="loading">
-        <p id="uploadText">
-            Uploading your data to the dark web...
-        </p>
-    </div>`
+        <div class="modal-inner-loading">
+            <img src="images/loading.svg" class="loading">
+            <p id="uploadText">
+                Uploading your data to the dark web...
+            </p>
+        </div>`
 
 
     setTimeout(() => {
@@ -34,12 +41,12 @@ consentForm.addEventListener('submit', function(e){
 
     setTimeout(() => {
         modalInner.innerHTML = `
-        <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
-        <p>We just sold the rights to your eternal soul.</p>
-        <div class="idiot-gif">
-            <img src="images/pirate.gif">
-        </div>`
+            <h2>Thanks <span class="modal-display-name">${name}</span>, you sucker! </h2>
+            <p>We just sold the rights to your eternal soul.</p>
+            <div class="idiot-gif">
+                <img src="images/pirate.gif">
+            </div>`
+        modalCloseBtn.disabled = false;
     }, 3000)
-// acceptBtn.addEventListener('click', () => {
     
 })
